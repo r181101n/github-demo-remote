e8bc52ae6de2334c07928c11d9429469c0fba0f1
r181101n <79794487+r181101n@users.noreply.github.com>
2021-09-01 00:00:20 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,20 @@
+#add implementation
+def add(x,y):
+    return x+y
+    #substract
+def subtract(x,y):#and here also
+    return x-y #on master
+    #multiply
+def multiply(x,y):
+  return x*y
+def divide(x,y):
+   if y==0:
+   return divide_by_0_error
+   else:
+
+    return x*y
+
+ 
+
+
+
